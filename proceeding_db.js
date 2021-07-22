02c553fecac378d57de99cd079c9bc20c9da3099
Howcanoe WANG <howcanoewang@gmail.com>
2021-07-22 15:47:50 +0900

    add db.js to store info

---
+++ proceeding_db.js
@@ -0,0 +1,23 @@
+var abs = [
+    {"authors": "Haozhou Wang <sup>1</sup>,Tang Li <sup>1</sup>,Erika Nishida <sup>1</sup>,Yuya Fukano <sup>1</sup>, Yoichiro Kato <sup>1</sup>, Wei Guo <sup>1</sup>",
+     "institudes": "1. Graduate School of Agricultural and Life Sciences, The University of Tokyo;Graduate School of Agricultural and Life Sciences, The University of Tokyo", 
+     "title": "Cost-efficient broccoli head phenotyping using aerial imagery and SfM-based weakly supervised learning",
+     "file": "aaa.pdf"},
+
+     {"authors": "Haozhou Wang <sup>1</sup>,Tang Li <sup>1</sup>,Erika Nishida <sup>1</sup>,Yuya Fukano <sup>1</sup>, Yoichiro Kato <sup>1</sup>, Wei Guo <sup>1</sup>",
+     "institudes": "1. Graduate School of Agricultural and Life Sciences, The University of Tokyo;Graduate School of Agricultural and Life Sciences, The University of Tokyo", 
+     "title": "Cost-efficient broccoli head phenotyping using aerial imagery and SfM-based weakly supervised learning",
+     "file": "aaa.pdf"},
+]
+
+var pos = [
+    {"authors": "Haozhou Wang <sup>1</sup>,Tang Li <sup>1</sup>,Erika Nishida <sup>1</sup>,Yuya Fukano <sup>1</sup>, Yoichiro Kato <sup>1</sup>, Wei Guo <sup>1</sup>",
+     "institudes": "1. Graduate School of Agricultural and Life Sciences, The University of Tokyo;Graduate School of Agricultural and Life Sciences, The University of Tokyo", 
+     "title": "Cost-efficient broccoli head phenotyping using aerial imagery and SfM-based weakly supervised learning",
+     "file": "aaa.pdf"},
+     
+     {"authors": "Haozhou Wang <sup>1</sup>,Tang Li <sup>1</sup>,Erika Nishida <sup>1</sup>,Yuya Fukano <sup>1</sup>, Yoichiro Kato <sup>1</sup>, Wei Guo <sup>1</sup>",
+     "institudes": "1. Graduate School of Agricultural and Life Sciences, The University of Tokyo;Graduate School of Agricultural and Life Sciences, The University of Tokyo", 
+     "title": "Cost-efficient broccoli head phenotyping using aerial imagery and SfM-based weakly supervised learning",
+     "file": "aaa.pdf"},
+]
