1958b3ab25eb2b55c11ff18708460b3529ddfdf1
HowcanoeWang <howcanoewang@gmail.com>
2021-09-06 15:25:24 +0900

    Update proceeding_db.js

--- proceeding_db.js
+++ proceeding_db.js
@@ -1,23 +1,23 @@
 var abs = [
-    {"authors": "Haozhou Wang <sup>1</sup>,Tang Li <sup>1</sup>,Erika Nishida <sup>1</sup>,Yuya Fukano <sup>1</sup>, Yoichiro Kato <sup>1</sup>, Wei Guo <sup>1</sup>",
-     "institudes": "1. Graduate School of Agricultural and Life Sciences, The University of Tokyo;Graduate School of Agricultural and Life Sciences, The University of Tokyo", 
-     "title": "Cost-efficient broccoli head phenotyping using aerial imagery and SfM-based weakly supervised learning",
+    {"authors": "Author A <sup>1</sup>, Author B <sup>1</sup>",
+     "institudes": "1. Affliation I", 
+     "title": "Title 1",
      "file": "aaa.pdf"},
 
-     {"authors": "Haozhou Wang <sup>1</sup>,Tang Li <sup>1</sup>,Erika Nishida <sup>1</sup>,Yuya Fukano <sup>1</sup>, Yoichiro Kato <sup>1</sup>, Wei Guo <sup>1</sup>",
-     "institudes": "1. Graduate School of Agricultural and Life Sciences, The University of Tokyo;Graduate School of Agricultural and Life Sciences, The University of Tokyo", 
-     "title": "Cost-efficient broccoli head phenotyping using aerial imagery and SfM-based weakly supervised learning",
-     "file": "aaa.pdf"},
+     {"authors": "Author C <sup>1</sup>, Author D <sup>1</sup>",
+     "institudes": "1. Affliation II", 
+     "title": "Title 2",
+     "file": "bbb.pdf"},
 ]
 
 var pos = [
-    {"authors": "Haozhou Wang <sup>1</sup>,Tang Li <sup>1</sup>,Erika Nishida <sup>1</sup>,Yuya Fukano <sup>1</sup>, Yoichiro Kato <sup>1</sup>, Wei Guo <sup>1</sup>",
-     "institudes": "1. Graduate School of Agricultural and Life Sciences, The University of Tokyo;Graduate School of Agricultural and Life Sciences, The University of Tokyo", 
-     "title": "Cost-efficient broccoli head phenotyping using aerial imagery and SfM-based weakly supervised learning",
-     "file": "aaa.pdf"},
-     
-     {"authors": "Haozhou Wang <sup>1</sup>,Tang Li <sup>1</sup>,Erika Nishida <sup>1</sup>,Yuya Fukano <sup>1</sup>, Yoichiro Kato <sup>1</sup>, Wei Guo <sup>1</sup>",
-     "institudes": "1. Graduate School of Agricultural and Life Sciences, The University of Tokyo;Graduate School of Agricultural and Life Sciences, The University of Tokyo", 
-     "title": "Cost-efficient broccoli head phenotyping using aerial imagery and SfM-based weakly supervised learning",
-     "file": "aaa.pdf"},
+    {"authors": "Author E <sup>1</sup>, Author F <sup>1</sup>",
+     "institudes": "1. Affliation III", 
+     "title": "Title 3",
+     "file": "ccc.pdf"},
+
+     {"authors": "Author G <sup>1</sup>, Author H <sup>1</sup>",
+     "institudes": "1. Affliation IV", 
+     "title": "Title 4",
+     "file": "ddd.pdf"},
 ]
